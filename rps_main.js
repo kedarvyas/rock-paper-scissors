165486b4267c2a49899ccbef643eacd2303bfc03
kedarvyas <kedar.m.vyas@gmail.com>
2021-07-16 18:50:08 -0500

    updating rps_main

--- rps_main.js
+++ rps_main.js
@@ -1,4 +1,132 @@
+let pScore = 0;
+let cScore = 0;
+let res;
+let choices = ['Rock', 'Paper', 'Scissors']
+const buttons = document.querySelectorAll('input')
+
+function computerPlay() {
+    return choices[Math.floor(Math.random() * choices.length)]; 
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+/*
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    res = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        pScore += 1
+        res = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + pScore + "<br>Computer score: " + cScore)
+
+        if (pScore == 5) {
+            res += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        res = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + pScore + "<br>Computer score: " + cScore)
+    }
+    else {
+        cScore += 1
+        res = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + pScore + "<br>Computer score: " + cScore)
+
+        if (cScore == 5) {
+            res += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = res
+    return
+}
+*/
+function playRound(playerSelection) {
+
+    //playerSelection = window.prompt("Please select an option: Rock, Paper, Scissors", '');
+    computerSelection = computerPlay();
+    res = "";
+
+    //playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock") {
+        if (computerSelection === "Paper") {
+            cScore++;
+            res = "Oops! Paper beats rock!" + "<br><br>Player Score: " + pScore + "<br>Computer Score: " + cScore;
+        }
+        else if (computerSelection === "Scissors") {
+            pScore++;
+            res = "Nice! Rock beats scissors!" + "<br><br>Player Score: " + pScore + "<br>Computer Score: " + cScore;
+        }
+        else {
+            res = "It's a Tie!" + "<br><br>Player Score: " + pScore + "<br>Computer Score: " + cScore;
+        }
+    }
+
+    else if (playerSelection == "paper") {
+        if (computerSelection === "Rock") {
+            pScore++;
+            res = "Woohoo! Paper beats rock!";
+        }
+        else if (computerSelection === "Scissors") {
+            cScore++;
+            res = "Unlucky. Scissors beats paper.";
+        }
+        else {
+            res = "Tie.";
+        }
+    }
+
+    else if (playerSelection == "scissors") {
+        if (computerSelection === "Rock") {
+            cScore++;
+            res = "Not this time. Rock beats scissors.";
+        }
+        else if (computerSelection === "Paper") {
+            pScore++;
+            res = "Sharp! Scissors beats paper!";
+        }
+        else {
+            res = "Tie!";
+        }
+    }
+
+    if(pScore == 5 || cScore == 5) {
+        if(pScore == 5) {
+            res = "Congrats! You beat the computer!";
+        }
+        else if(cScore == 5) {
+            res = "You were beat by the computer! You'll get em next time.";
+        }
+        else {
+            res = "Tie game. Try again.";
+        }
+    }
+
+    document.getElementById('result').innerHTML = res
+    return
+
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
+/*
+
 let choices = ['Rock', 'Paper', 'Scissors'];
+let res;
+
 const buttons = document.querySelectorAll('input');
 
 function computerPlay() {
@@ -6,11 +134,11 @@ function computerPlay() {
     return res;
 }
 
-let playerSelection;
-let computerSelection;
-var pScore = 0;
-var cScore = 0;
-const res;
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
 function playRound(playerSelection, computerSelection) {
 
@@ -23,49 +151,59 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock") {
         if (computerSelection === "Paper") {
             cScore++;
-            return "Oops! Paper beats rock!";
+            res = "Oops! Paper beats rock!";
         }
         else if (computerSelection === "Scissors") {
             pScore++;
-            return "Nice! Rock beats scissors!";
+            res = "Nice! Rock beats scissors!";
         }
         else {
-            return "It's a Tie!";
+            res = "It's a Tie!";
         }
     }
 
     else if (playerSelection == "paper") {
         if (computerSelection === "Rock") {
             pScore++;
-            return "Woohoo! Paper beats rock!";
+            res = "Woohoo! Paper beats rock!";
         }
         else if (computerSelection === "Scissors") {
             cScore++;
-            return "Unlucky. Scissors beats paper.";
+            res = "Unlucky. Scissors beats paper.";
         }
         else {
-            return "Tie.";
+            res = "Tie.";
         }
     }
 
     else if (playerSelection == "scissors") {
         if (computerSelection === "Rock") {
             cScore++;
-            return "Not this time. Rock beats scissors.";
+            res = "Not this time. Rock beats scissors.";
         }
         else if (computerSelection === "Paper") {
             pScore++;
-            return "Sharp! Scissors beats paper!";
+            res = "Sharp! Scissors beats paper!";
         }
         else {
-            return "Tie!";
+            res = "Tie!";
         }
     }
 
-    
+    if(pScore == 5 || cScore == 5) {
+        if(pScore == 5) {
+            res = "Congrats! You beat the computer!";
+        }
+        else if(cScore == 5) {
+            res = "You were beat by the computer! You'll get em next time.";
+        }
+        else {
+            res = "Tie game. Try again.";
+        }
+    }
 
     document.getElementById('result').innerHTML = result
-    return;
+    return
 
 }
 
@@ -75,11 +213,8 @@ buttons.forEach(button => {
     })
 })
 
-function checkWinner() {
 
-}
 
-/*
         function game() {
             var i;
             for (i = 0; i < 5; i++) {
